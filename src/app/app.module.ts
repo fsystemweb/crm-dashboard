2cf9bbfc837664afd522fc1089af4d5ccd04fb2e
diazfa <facundoezequiel.diaz@intrum.com>
2024-10-24 14:00:04 +0200

    add icon module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, CommonModule, CoreModule],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
 })
 export class AppModule {}
--- src/app/features/customers/customers.module.ts
+++ src/app/features/customers/customers.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersPageComponent } from './page/customers-page.component';
 import { StatsCardsComponent } from './components/stats-cards/stats-cards.component';
+import { IconModule } from 'src/app/shared/components/icons/icon.module';
 
 @NgModule({
-  imports: [CustomersRoutingModule, CommonModule],
+  imports: [CustomersRoutingModule, CommonModule, IconModule],
   declarations: [CustomersPageComponent, StatsCardsComponent],
 })
 export class CustomersModule {}
