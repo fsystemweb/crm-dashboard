644d7bf7fc576c063e918c88963bc292efafe0d4
Facundo Ezequiel Diaz Cappella <43161950+fsystemweb@users.noreply.github.com>
2024-10-24 16:29:37 +0200

    add customer table (#4)
    Co-authored-by: diazfa <facundoezequiel.diaz@intrum.com>

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,10 @@ import { LayoutComponent } from './components/layout/layout.component';
 import { RouterLinkWithHref, RouterModule } from '@angular/router';
 import { IconModule } from '../shared/components/icons/icon.module';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [RouterModule, RouterLinkWithHref, CommonModule, IconModule],
+  imports: [RouterModule, RouterLinkWithHref, CommonModule, IconModule, FormsModule],
   declarations: [NavbarComponent, SidebarComponent, LayoutComponent],
   exports: [NavbarComponent, SidebarComponent, LayoutComponent],
   providers: [],
--- src/app/features/customers/customers.module.ts
+++ src/app/features/customers/customers.module.ts
@@ -4,9 +4,13 @@ import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersPageComponent } from './page/customers-page.component';
 import { StatsCardsComponent } from './components/stats-cards/stats-cards.component';
 import { IconModule } from 'src/app/shared/components/icons/icon.module';
+import { CustomerTableComponent } from './components/customer-table/customer-table.component';
+import { FormsModule } from '@angular/forms';
+import { TablePaginationComponent } from './components/table-pagination/table-pagination.component';
+import { CustomerFiltersSectionComponent } from './components/customer-filters-section/customer-filters-section.component';
 
 @NgModule({
-  imports: [CustomersRoutingModule, CommonModule, IconModule],
-  declarations: [CustomersPageComponent, StatsCardsComponent],
+  imports: [CustomersRoutingModule, CommonModule, IconModule, FormsModule],
+  declarations: [CustomersPageComponent, StatsCardsComponent, CustomerTableComponent, TablePaginationComponent, CustomerFiltersSectionComponent],
 })
 export class CustomersModule {}
