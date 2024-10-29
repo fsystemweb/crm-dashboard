77972fff84e8e8192017f5609a078da2fa995af8
diazfa <facundoezequiel.diaz@intrum.com>
2024-10-29 17:09:14 +0100

    add more test

---
+++ src/app/core/components/navbar/navbar.component.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed } from '@angular/core/testing';
+
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideMockStore } from '@ngrx/store/testing';
+
+import { NavbarComponent } from './navbar.component';
+import { getUserInfo } from 'src/app/state/entities/user-info/user-info.selectors';
+import { FormsModule } from '@angular/forms';
+import { IconModule } from 'src/app/shared/components/icons/icon.module';
+describe('NavbarComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FormsModule, IconModule],
+      declarations: [NavbarComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideMockStore({
+          selectors: [
+            {
+              selector: getUserInfo,
+              value: {
+                loadedUserInfo: true,
+                userInfo: {
+                  fullname: 'Alf',
+                  role: 'CEO',
+                  picture: 'alf.jpeg',
+                },
+              },
+            },
+          ],
+        }),
+      ],
+    }).compileComponents();
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(NavbarComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello Alf');
+  });
+});
