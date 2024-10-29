e554ef2364fd4612c665905e53279bc7226786c6
diazfa <facundoezequiel.diaz@intrum.com>
2024-10-29 17:06:45 +0100

    update test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,12 +4,32 @@ import { RouterModule } from '@angular/router';
 import { CoreModule } from './core/core.module';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
+import { provideMockStore } from '@ngrx/store/testing';
+import { getUserInfo } from './state/entities/user-info/user-info.selectors';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterModule.forRoot([]), CoreModule],
       declarations: [AppComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideMockStore({
+          selectors: [
+            {
+              selector: getUserInfo,
+              value: {
+                loadedUserInfo: true,
+                userInfo: {
+                  fullname: 'Alf',
+                  role: 'CEO',
+                  picture: 'alf.jpeg',
+                },
+              },
+            },
+          ],
+        }),
+      ],
     }).compileComponents();
   });
 
@@ -17,10 +37,10 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello Guest');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello Alf');
   });
 
-  it('should have as title \'crm-dashboard\'', () => {
+  it("should have as title 'crm-dashboard'", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('crm-dashboard');
