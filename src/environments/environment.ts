25a40bf6979bfebf890a82bb93b8775ca3336875
fed <facu.cappella@gmail.com>
2025-03-01 12:07:25 +0100

    fix api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ import { EnvInterface } from './environment.interface';
 
 export const environment: EnvInterface = {
   production: false,
-  apiUrl: 'https://crm-dashboard-api-6yw3.onrender.com:10000',
+  apiUrl: 'https://crm-dashboard-api-6yw3.onrender.com',
 };
